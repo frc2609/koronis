5739ac73759f9e0ee8f7389d8eb36ed7eac5f8b4
Shaun Wu <Alexbay218@gmail.com>
2019-09-07 18:32:11 -0400

    Fix bug where engine type checking fails because of minification

--- src/engine/EngineWorkerAssembler.js
+++ src/engine/EngineWorkerAssembler.js
@@ -3,15 +3,15 @@ export default class EngineWorkerAssembler {
     var initCode = '';
     var onMessageCode = '';
     for(var i = 0;i < engineComponentList.length;i++) {
-      var currInitCode = engineComponentList[i].prototype.workerInit.toString();
+      var currInitCode = engineComponentList[i].ec.prototype.workerInit.toString();
       currInitCode = currInitCode.substring(currInitCode.indexOf("{") + 1, currInitCode.lastIndexOf("}"));
       currInitCode += '\n';
-      initCode += 'console.log(\'[Engine] Engine Component ' + engineComponentList[i].name + ' initializing\');\n' + currInitCode + 'console.log(\'[Engine] Engine Component ' + engineComponentList[i].name + ' initialized\');\n';
+      initCode += 'console.log(\'[Engine] Engine Component ' + engineComponentList[i].type + ' initializing\');\n' + currInitCode + 'console.log(\'[Engine] Engine Component ' + engineComponentList[i].type + ' initialized\');\n';
 
-      var currOnMessageCode = engineComponentList[i].prototype.workerReply.toString();
+      var currOnMessageCode = engineComponentList[i].ec.prototype.workerReply.toString();
       currOnMessageCode = currOnMessageCode.substring(currOnMessageCode.indexOf("{") + 1, currOnMessageCode.lastIndexOf("}"));
       currOnMessageCode += '\n';
-      onMessageCode += 'if(requestMessage.engineComponentType === \'' + engineComponentList[i].name + '\'){\n' + currOnMessageCode + '}\n';
+      onMessageCode += 'if(requestMessage.engineComponentType === \'' + engineComponentList[i].type + '\'){\n' + currOnMessageCode + '}\n';
     }
 
     var code = initCode + 'onmessage = (message) => {\nvar requestMessage = message.data;\nconsole.log(\'[Engine] Recieved Request: \' + JSON.stringify(requestMessage));\n' + onMessageCode + '};';
