6e3055e5eabd8ccbbaa3384a3b7d79f7fffeeb0d
Shaun Wu <Alexbay218@gmail.com>
2019-09-07 13:09:07 -0400

    Add webworker and fixed package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import webWorkerDriver from 'engine/webWorkerDriver';
 
 import MainAppBar from "ui-tree/MainAppBar";
 import Main from "ui-tree/Main";
@@ -7,9 +8,16 @@ import Main from "ui-tree/Main";
 import 'App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+const engineWorkerDriver = new webWorkerDriver();
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {ewd: engineWorkerDriver};
+  }
+  componentDidMount() {
+
+    this.state.ewd.postTestMessage();
   }
   render() {return (
     <div className="App">
