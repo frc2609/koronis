c003329a134517a6b4287913d3ce085466501649
Shaun Wu <alexbay218@gmail.com>
2019-10-24 22:51:03 -0400

    Fix importing js

--- src/package/PackageCollector.js
+++ src/package/PackageCollector.js
@@ -5,26 +5,38 @@ var initialized = false;
 
 var geval = eval;
 var botStateDefinition = {};
+var buttonDefinitions = [];
+var eventDefinitions = [];
 
 export const init = async () => {
   try {
-    var aY = (await axios.get(packageUrl + 'index.json')).data.replace(/'/g,'\"');
-    var availableYears = JSON.parse(aY).availableYears;
+    var availableYears = (await axios.get(packageUrl + 'index.json')).data.availableYears;
     store.set('package/availableYears', availableYears);
     for(var i = 0;i < availableYears.length;i++) {
-      var bSD = (await axios.get(packageUrl + availableYears[i] + '/bot.js')).data.replace(/[\r\n]/g,'').replace(/\s+/g,' ').replace(/'/g,'\"');
-      //.replace(//g,'')
-      //eval(bSD)
-      //eval("var botStateDefinition = { pos: { x: 0, y: 0, t: 0 }, currentZones: [], previousZones: [], cargo: false, hatch: false}")
-      console.log(String(bSD))
-      console.log(botStateDefinition);
-      var cP = (await axios.get(packageUrl + availableYears[i] + '/color.json')).data.replace(/'/g,'\"');
-      var colorPalette = JSON.parse(cP).colorPalette;
-      store.set('package/' + availableYears[i] + 'colorPalette', colorPalette);
+      eval('import(\"' + packageUrl + availableYears[i] + '/bot.js\")').then((bSD) => {
+        botStateDefinition = bSD.default;
+        store.set('package/' + availableYears[i] + '/botStateDefinition', botStateDefinition);
+      });
+      eval('import(\"' + packageUrl + availableYears[i] + '/buttons.js\")').then((bD) => {
+        buttonDefinitions = bD.default;
+        store.set('package/' + availableYears[i] + '/buttonDefinitions', buttonDefinitions);
+      });
+      var colorPalette = (await axios.get(packageUrl + availableYears[i] + '/color.json',{responseType: 'json'})).data.colorPalette;
+      store.set('package/' + availableYears[i] + '/colorPalette', colorPalette);
+      eval('import(\"' + packageUrl + availableYears[i] + '/events.js\")').then((eD) => {
+        eventDefinitions = eD.default;
+        console.log(eventDefinitions);
+        store.set('package/' + availableYears[i] + '/eventDefinitions', eventDefinitions);
+      });
+      var fieldDefinition = (await axios.get(packageUrl + availableYears[i] + '/field.json',{responseType: 'json'})).data;
+      store.set('package/' + availableYears[i] + '/fieldDefinition', fieldDefinition);
+      var gameDefinition = (await axios.get(packageUrl + availableYears[i] + '/game.json',{responseType: 'json'})).data;
+      store.set('package/' + availableYears[i] + '/gameDefinition', gameDefinition);
+      console.log('[Package] Packages for ' + availableYears[i] + ' loaded');
     }
   }
   catch(err) {
-    console.log('[Package] cannot get latest packages');
+    console.log('[Package] Cannot get latest packages');
   }
   initialized = true;
 }
@@ -38,7 +50,32 @@ export const get = async () => {
   for(var i = 0;i < result.availableYears.length;i++) {
     var currYear = result.availableYears[i];
     result[currYear] = {};
-    result[currYear].colorPalette =  store.get('package/' + currYear + 'colorPalette');
+    result[currYear].botStateDefinition = store.get('package/' + currYear + '/botStateDefinition');
+    result[currYear].buttonDefinitions = store.get('package/' + currYear + '/buttonDefinitions');
+    result[currYear].colorPalette = store.get('package/' + currYear + '/colorPalette');
+    result[currYear].eventDefinitions = store.get('package/' + currYear + '/eventDefinitions');
+    result[currYear].fieldDefinition = store.get('package/' + currYear + '/fieldDefinition');
+    result[currYear].gameDefinition = store.get('package/' + currYear + '/gameDefinition');
+  }
+  return result;
+}
+
+export const getByYear = async (inYear) => {
+  if(!initialized) {
+    await init();
+  }
+  var result = {};
+  var availableYears = store.get('package/availableYears');
+  for(var i = 0;i < availableYears.length;i++) {
+    var currYear = availableYears[i];
+    if(currYear == inYear) {
+      result.botStateDefinition = store.get('package/' + currYear + '/botStateDefinition');
+      result.buttonDefinitions = store.get('package/' + currYear + '/buttonDefinitions');
+      result.colorPalette = store.get('package/' + currYear + '/colorPalette');
+      result.eventDefinitions = store.get('package/' + currYear + '/eventDefinitions');
+      result.fieldDefinition = store.get('package/' + currYear + '/fieldDefinition');
+      result.gameDefinition = store.get('package/' + currYear + '/gameDefinition');
+    }
   }
   return result;
 }
