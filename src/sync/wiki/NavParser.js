eb9109174382beaadf44bd8a629b4466a1851c27
Shaun Wu <Alexbay218@gmail.com>
2020-08-03 22:27:15 -0400

    add wiki parser

--- src/sync/wiki/NavParser.js
+++ src/sync/wiki/NavParser.js
@@ -2,18 +2,35 @@ import Config from 'config/Config';
 
 var axios = require('axios');
 
+const parseLine = (line) => {
+  var res = ['', ''];
+  res[0] = line.substring(line.indexOf('[')+1,line.indexOf(']'));
+  res[1] = line.substring(line.indexOf('(')+1,line.indexOf(')'));
+  return res;
+};
+
 export const parseNav = async () => {
   var nav = (await axios(Config.wikiUrl + 'navigation.md')).data;
   var arr = [];
-  var res = {};
+  var res = [];
   nav = nav.substring(nav.indexOf('# KSS Wiki\n')+12,nav.indexOf('\n[Edit]'));
   arr = nav.split('\n');
   var currScope = '';
   for(var i = 0;i < arr.length;i++) {
-    if(arr[i] !== '') {
+    if(arr[i] !== '' && !arr[i].includes('#')) {
       var nestedCount = arr[i].indexOf('*') < 0 ? 0 : Math.floor(arr[i].indexOf('*') / 2);
+      console.log(arr[i])
+      console.log(parseLine(arr[i]));
       if(nestedCount === 0) {
-        currScope = arr[i];
+        var parsed = parseLine(arr[i]);
+        currScope = parsed[0]
+        res.push({
+          path: parsed[0],
+          value: parsed[1]
+        });
+      }
+      else {
+        var currArr = arr[i].split('\');
       }
     }
   }
