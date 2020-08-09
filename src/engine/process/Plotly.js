418a5130e3c76c9619ea26796755b8e7df4a8ac6
Shaun Wu <Alexbay218@gmail.com>
2020-08-09 12:46:50 -0400

    Scrap unneeded Plotly traces

---
+++ src/engine/process/Plotly.js
@@ -0,0 +1,25 @@
+var Plotly = require('plotly.js/lib/core');
+
+Plotly.register([
+  require('plotly.js/lib/scatter'),
+  require('plotly.js/lib/bar'),
+  require('plotly.js/lib/box'),
+  require('plotly.js/lib/heatmap'),
+  require('plotly.js/lib/histogram'),
+  require('plotly.js/lib/contour'),
+  require('plotly.js/lib/image'),
+  require('plotly.js/lib/pie'),
+  require('plotly.js/lib/sunburst'),
+  require('plotly.js/lib/treemap'),
+  require('plotly.js/lib/surface'),
+  require('plotly.js/lib/funnelarea'),
+  require('plotly.js/lib/splom'),
+  require('plotly.js/lib/indicator'),
+  require('plotly.js/lib/aggregate'),
+  require('plotly.js/lib/filter'),
+  require('plotly.js/lib/groupby'),
+  require('plotly.js/lib/sort'),
+  require('plotly.js/lib/calendars')
+]);
+
+module.exports = Plotly;
