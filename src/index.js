707c0ae84058b15ca85df972bc9d030bb49ef3c5
Shaun Wu <alexbay218@gmail.com>
2022-03-02 01:15:39 -0500

    Remove sw register in index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ import 'fontsource-roboto';
 
 import App from './App';
 import Splash from './Splash';
-import * as ServiceWorker from './ServiceWorkerRegistration';
 
 import * as DefaultSettings from 'config/DefaultSettings';
 import * as Debug from 'config/Debug';
 import * as Sync from 'sync/Sync';
 import * as Db from 'db/Db';
-
-const swUpdateAvailable = new Event('swupdateavailable');
-const swOfflineReady = new Event('swofflineready');
 
 DefaultSettings.setDefaults();
 ReactDOM.render(<Splash />, document.getElementById('root'));
@@ -22,18 +18,4 @@ Debug.init();
 Db.init().then(() => {
   ReactDOM.render(<App />, document.getElementById('root'));
   Sync.init();
-});
-
-ServiceWorker.register({
-  onUpdate: (reg) => {
-    this.reg = reg;
-    window.dispatchEvent(swUpdateAvailable);
-  },
-  onSuccess: () => { window.dispatchEvent(swOfflineReady); },
-  onReady: (reg) => {
-    if(reg.waiting !== null) {
-      this.reg = reg;
-      window.dispatchEvent(swUpdateAvailable);
-    }
-  },
 });
