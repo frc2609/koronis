d922bf5b9c3e90cb6fda49c219a1192cf9c5d97a
Shaun Wu <alexbay218@gmail.com>
2022-12-06 19:14:07 -0500

    Use built in webserver

--- playwright.config.js
+++ playwright.config.js
@@ -33,12 +33,19 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  webServer: {
+    command: 'npm run start',
+    url: 'https://localhost:3000',
+    ignoreHTTPSErrors: true,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
