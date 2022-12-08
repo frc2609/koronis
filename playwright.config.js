b2d8d858001f7a4b8f434a28c01f375c8c50b6cf
Shaun <alexbay218@gmail.com>
2022-12-08 15:26:09 -0500

    Rename and switch to matrix parallelization

--- playwright.config.js
+++ playwright.config.js
@@ -99,27 +99,27 @@ const config = {
 
     /* Test against mobile viewports. */
     {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
-    },
-    {
-      name: 'Mobile Safari',
-      use: {
-        ...devices['iPhone 12'],
-      },
-    },
-    {
       name: 'Mobile Landscape Chrome',
       use: {
         ...devices['Pixel 5 landscape'],
       },
     },
     {
+      name: 'Mobile Portrait Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
       name: 'Mobile Landscape Safari',
       use: {
         ...devices['iPhone 12 landscape'],
+      },
+    },
+    {
+      name: 'Mobile Portrait Safari',
+      use: {
+        ...devices['iPhone 12'],
       },
     },
   ],
